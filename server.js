09186edecf5cd3a555e2a723dce2f15496d0a019
Violet <42684875+Violetce@users.noreply.github.com>
2018-12-12 19:19:10 -0600

    added routes to basic server

--- server.js
+++ server.js
@@ -8,6 +8,44 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Routes
+app.get("/", function(req, res) {
+
+});
+
+// Displays all characters
+app.get("/api/reservations", function(req, res) {
+  return res.json(reservations);
+});
+
+// Displays a single reservation, or shows "No reservation found"
+app.get("/api/reservations/:reservation", function(req, res) {
+  var chosen = req.params.reservation;
+
+  console.log(chosen);
+
+  for (var i = 0; i < reservations.length; i++) {
+    if (chosen === reservations[i].routeName) {
+      return res.json(reservations[i]);
+    }
+  }
+
+  return res.send("No reservation found");
+
+});
+
+// Create New Reservations - takes in JSON input
+app.post("/api/reservations", function(req, res) {
+  var newreservation = req.body;
+
+  console.log(newreservation);
+
+  reservations.push(newreservation);
+
+  res.json(newreservation);
+});
+
+
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
